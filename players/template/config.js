1efa08893b45835e7759df936b2cec7004a894a7
byte <141379530+ahmedelgharibb@users.noreply.github.com>
2025-03-15 23:00:32 +0200

    Update config.js

--- players/template/config.js
+++ players/template/config.js
@@ -54,6 +54,12 @@ const config = {
             year: "2020",
             title: "International Recognition",
             description: "Gained international recognition after winning a global championship."
+        },
+
+        {
+            year: "2020",
+            title: "International Recognition",
+            description: "Gained international recognition after winning a global championship."
         }
     ],
  
