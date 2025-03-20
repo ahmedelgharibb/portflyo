70f634f2f569f18b18f159e6d75567bac54571fc
byte <141379530+ahmedelgharibb@users.noreply.github.com>
2025-03-20 03:00:15 +0200

    Update config.js

--- players/template1/config.js
+++ players/template1/config.js
@@ -13,7 +13,7 @@ const config = {
 
     // Site Branding
     branding: {
-        name: "Sports<span>Scout</span>",
+        name: "Sport<span>Scout</span>",
         instagram: "https://www.instagram.com/portflyo/"
     },
 
